0ba0b0e975d6fc1a0405b5c4a247242d586f8a46
byungi <zeronin2@gmail.com>
2022-12-10 23:57:56 +0900

    wip

--- scripts/publish_packages_to_npm.ts
+++ scripts/publish_packages_to_npm.ts
@@ -8,28 +8,28 @@ const exec = promisify(_exec)
 main()
 
 async function main() {
-    const prevPkgs = new Set(Object.keys(JSON.parse((await exec('pnpm access ls-packages @noonnu')).stdout)))
-    const nextPkgs = new Set(
-        (JSON.parse((await exec('pnpm list --json -r')).stdout) as { name: string }[]).map(pkg => pkg.name)
-    )
+    // const prevPkgs = new Set(Object.keys(JSON.parse((await exec('pnpm access ls-packages @noonnu')).stdout)))
+    // const nextPkgs = new Set(
+    //     (JSON.parse((await exec('pnpm list --json -r')).stdout) as { name: string }[]).map(pkg => pkg.name)
+    // )
 
-    log('존재하지 않는 패키지를 deprecate합니다.')
-    const deprecatedCnt = await lastValueFrom(
-        from(prevPkgs).pipe(
-            filter(pkg => !nextPkgs.has(pkg)),
-            mergeMap(
-                pkg =>
-                    exec(`pnpm deprecate ${pkg}@"*" "deprecated"`).then(
-                        () => log('~>', pkg),
-                        // 이미 deprecated된 패키지는 에러가 발생합니다. 무시합니다.
-                        console.warn
-                    ),
-                CPUS_LEN
-            ),
-            count()
-        )
-    )
-    log(`총 ${deprecatedCnt}개의 패키지를 deprecate했습니다.`)
+    // log('존재하지 않는 패키지를 deprecate합니다.')
+    // const deprecatedCnt = await lastValueFrom(
+    //     from(prevPkgs).pipe(
+    //         filter(pkg => !nextPkgs.has(pkg)),
+    //         mergeMap(
+    //             pkg =>
+    //                 exec(`pnpm deprecate ${pkg}@"*" "deprecated"`).then(
+    //                     () => log('~>', pkg),
+    //                     // 이미 deprecated된 패키지는 에러가 발생합니다. 무시합니다.
+    //                     console.warn
+    //                 ),
+    //             CPUS_LEN
+    //         ),
+    //         count()
+    //     )
+    // )
+    // log(`총 ${deprecatedCnt}개의 패키지를 deprecate했습니다.`)
 
     log('패키지들을 publish합니다.')
     await exec('pnpm publish -r --access public --no-git-checks')
